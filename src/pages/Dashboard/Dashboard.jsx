a3ffefb4604bd071ac2ef779ed798031c5a3cf8c
iRTerence <terencetan@hotmail.ca>
2023-05-29 07:27:22 -0400

    removed api call from dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,50 +7,49 @@ import Tables from "../../components/Tables/Tables";
 // import { getUsers, callDeviceAuditApi } from "../../util/http";
 const Home = () => {
   async function callDeviceAuditApi() {
-
     var raw = JSON.stringify({
-    
-        "location": "",
-        "franchiseeid": "117",
-        "firstname": "",
-        "lastname": "",
-        "phoneno": "",
-        "cityname": "",
-        "stateid": "",
-        "countryid": "",
-        "other1": "",
-        "other2": "",
-        "other3": "",
-        "usertype": "0",
-        "startuserid": "0",
-        "limit": "0"
-    
+      location: "",
+      franchiseeid: "117",
+      firstname: "",
+      lastname: "",
+      phoneno: "",
+      cityname: "",
+      stateid: "",
+      countryid: "",
+      other1: "",
+      other2: "",
+      other3: "",
+      usertype: "0",
+      startuserid: "0",
+      limit: "0",
     });
-    
+
     var myHeaders = new Headers();
     myHeaders.append("Cookie", "PHPSESSID=rvrlpdaj6v419pl7op1p8j1o0t");
-    
+
     var formdata = new FormData();
     formdata.append("svc", "app_2003");
     formdata.append("pid", "ListCustomerByPrograms");
     formdata.append("dat", raw);
-    
+
     var requestOptions = {
       retries: 10,
       retryDelay: 10000,
-      method: 'POST',
-      headers: { 'Content-Type': 'multipart/form-data' },
+      method: "POST",
+      headers: { "Content-Type": "multipart/form-data" },
       body: formdata,
     };
-    
-    fetch("https://dev2.4pay.ca/mophonecrm/www/php/Workflows/services.php?output=json", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-      
-    }
 
-    callDeviceAuditApi()
+    fetch(
+      "https://dev2.4pay.ca/mophonecrm/www/php/Workflows/services.php?output=json",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
+  // callDeviceAuditApi()
   return (
     <>
       <div className="home">
